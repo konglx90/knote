6fe9918cb5b06c56785410500c77aaf201e7c6e0
lingxing kong <kong@lingxingdeMacBook-Pro.local>
2018-06-30 11:00:46 +0800

    update

---
+++ sea/algorithms/find-the-min-item-in-array/index.js
@@ -0,0 +1,15 @@
+/**
+ * 寻找有序数组进行移动后的数组中的最小元素
+ * 形如:
+ * [1, 2, 3, 4, 5, 6]
+ * [2, 3, 4, 5, 6, 1]
+ */
+
+/**
+ * 二分的某种变形
+ */
+const findTheMinInArray = (arr) => {
+
+}
+
+module.exports = findTheMinInArray;
